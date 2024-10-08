b7d3cbc6722f06ebd71bc4619c1e9a3b87b80e33
Mikasa33 <mikasa33@qq.com>
2024-10-08 23:47:01 +0800

    chore(project): :hammer: 一些小改动

---
+++ apps/web/src/pages/about.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="m-16px">
+    <NCard :bordered="false">
+      <NButton>
+        About
+      </NButton>
+    </NCard>
+  </div>
+</template>
