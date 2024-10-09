387c3c5d0dce4a380551f00b855e613775d0844f
Mikasa33 <mikasa33@qq.com>
2024-10-09 16:00:42 +0800

    feat(project): :sparkles: 新增加载进度条

--- apps/web/src/router.ts
+++ apps/web/src/router.ts
@@ -1,7 +1,13 @@
 import type { App } from 'vue'
+import { usePreference } from '@a4v/preference'
+import { useNProgress } from '@vueuse/integrations/useNProgress'
 import { setupLayouts } from 'virtual:generated-layouts'
 import { createRouter, createWebHistory } from 'vue-router/auto'
 import { handleHotUpdate, routes } from 'vue-router/auto-routes'
+
+const { start, done } = useNProgress(null, {
+  showSpinner: false,
+})
 
 export const router = createRouter({
   history: createWebHistory(),
@@ -11,6 +17,18 @@ export const router = createRouter({
 if (import.meta.hot) {
   handleHotUpdate(router)
 }
+
+router.beforeEach(() => {
+  const { info } = usePreference()
+  if (info.value.page.loadingBar)
+    start()
+})
+
+router.afterEach(() => {
+  const { info } = usePreference()
+  if (info.value.page.loadingBar)
+    done()
+})
 
 export function setupRouter(app: App) {
   app.use(router)
