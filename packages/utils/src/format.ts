cd551791c4b9bd49f3282349c6c576859bf3a24c
Mikasa33 <mikasa33@qq.com>
2024-10-09 19:18:42 +0800

    refactor(utils): :recycle: 筛选公共工具函数

--- packages/utils/src/format.ts
+++ packages/utils/src/format.ts
@@ -1,5 +1,5 @@
 import type { Numeric } from './is'
-import { isNil } from 'lodash-es'
+import { isFunction, isNil } from 'lodash-es'
 import { isNumeric } from './is'
 
 export function addUnit(value?: Numeric): string | undefined {
@@ -9,12 +9,12 @@ export function addUnit(value?: Numeric): string | undefined {
   return undefined
 }
 
-// export function getMaybeFuncValue(val: any, params: any) {
-//   if (isFunction(val)) {
-//     return val(params)
-//   }
-//   return val
-// }
+export function getMaybeFuncValue(val: any, params: any) {
+  if (isFunction(val)) {
+    return val(params)
+  }
+  return val
+}
 
 export function convertPath2Object(path: string, value?: any) {
   const obj: any = {}
--- packages/utils/src/vue/index.ts
+++ packages/utils/src/vue/index.ts
@@ -1,4 +1,2 @@
 export * from './create-injection-key'
-export * from './prop'
 export * from './render'
-export * from './with-install'
