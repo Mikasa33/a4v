b3cab2a14b48ac8c57f5cb262394144808d0150c
Mikasa33 <mikasa33@qq.com>
2024-10-11 18:35:26 +0800

    fix(docs): :bug: 修复引入错误组件的问题

--- docs/.vitepress/components/Block.tsx
+++ docs/.vitepress/components/Block.tsx
@@ -1,4 +1,3 @@
-import { SsrContext } from '@css-render/vue3-ssr'
 import { NCard } from 'naive-ui'
 import { defineComponent } from 'vue'
 import ConfigProvider from './ConfigProvider.vue'
@@ -7,13 +6,11 @@ export function Block(Component: any) {
   return defineComponent({
     render() {
       return (
-        <SsrContext>
-          <ConfigProvider>
-            <NCard themeOverrides={{ borderRadius: '8px' }}>
-              <Component />
-            </NCard>
-          </ConfigProvider>
-        </SsrContext>
+        <ConfigProvider>
+          <NCard themeOverrides={{ borderRadius: '8px' }}>
+            <Component />
+          </NCard>
+        </ConfigProvider>
       )
     },
   })
